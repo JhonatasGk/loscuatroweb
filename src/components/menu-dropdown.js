1523e4579a95752cab7a1a9bdbf3134cf9f7b72a
JhonatasGk <jhonatasgr@gmail.com>
2021-09-08 23:45:38 -0300

    sub

--- src/components/menu-dropdown.js
+++ src/components/menu-dropdown.js
@@ -1,5 +1,6 @@
 window.addEventListener('mouseup', function(event){
 	var box = document.getElementById('aba');
+	// eslint-disable-next-line eqeqeq
 	if (event.target != box && event.target.parentNode != box){
         box.style.display = 'none';
     }
